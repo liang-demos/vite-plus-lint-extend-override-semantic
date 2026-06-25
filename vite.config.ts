74634a7a19df793499dbb580e68b44c5b6ceaff1
Liang Mi <github@liangmi.dev>
2026-06-25 11:01:17 +0800

    init

---
+++ vite.config.ts
@@ -0,0 +1,26 @@
+import { defineConfig } from "vite-plus";
+
+export default defineConfig({
+  lint: {
+    // `extends` already works, though it cannot specific files.
+    extends: [
+      {
+        rules: {
+          "no-console": "error",
+        },
+      },
+    ],
+    plugins: ["unicorn", "eslint"],
+    overrides: [
+      {
+        files: ["**/*.vue"],
+        // I didn't copy all the plugins,, but `no-console` is still working
+        // `overrides` use `merge semantic there`
+        plugins: ["vue"],
+        rules: {
+          "vue/no-export-in-script-setup": "error"
+        }
+      }
+    ]
+  },
+});
